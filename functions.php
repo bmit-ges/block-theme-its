4f2d15043f196bfc2e3f79ddef898d5e1cb13115
ssiissii100 <sibylle.gehring@espas.ch>
2023-03-14 09:36:59 +0100

    stylesheet + import stylesheet

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
+
+    /*function that adds css from the assets*/
+    function stylesheet_css() {
+        wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/assets/css/stylesheet.css' );
+    }
+    add_action( 'wp_enqueue_scripts', 'stylesheet_css' );
+
+    /*function to add scripts*/
     function examples_script() {
         wp_enqueue_script( 'examples', get_template_directory_uri() . '/assets/js/examples.js', array( 'jquery' ), '1.0.0', true );
     }
